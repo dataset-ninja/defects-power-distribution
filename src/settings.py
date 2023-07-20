e77792f37557799a85cfe0d0de231d342f1f4b62
Ars Vladimirov <grokhael@gmail.com>
2023-07-20 19:20:06 +0600

    update custom data

--- src/settings.py
+++ src/settings.py
@@ -1,22 +1,35 @@
 from typing import Dict, List, Optional, Union
 
-from dataset_tools.templates import AnnotationType, CVTask, Industry, License
+from dataset_tools.templates import (
+    AnnotationType,
+    Category,
+    CVTask,
+    Domain,
+    Industry,
+    License,
+    Research,
+)
 
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = "DefectsPDC"
-PROJECT_NAME_FULL: Optional[str] = "DefectsPDC: Defects in Power Distribution Components"
+PROJECT_NAME: str = "Defects in Power Distribution Components"
+PROJECT_NAME_FULL: Optional[str] = "Defects in Power Distribution Components"
 
 ##################################
 # * After uploading to instance ##
 ##################################
 LICENSE: License = License.CC_BY_4_0()
-INDUSTRIES: List[Industry] = [Industry.Energy()]
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Energy()]
+CATEGORY: Category = Category.EnergyAndUtilities()
+
 CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
 ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
-RELEASE_YEAR: int = 2020
+RELEASE_DATE: Optional[str] = "2020-08-05"  # e.g. "YYYY-MM-DD"
+if RELEASE_DATE is None:
+    RELEASE_YEAR: int = None
+
 HOMEPAGE_URL: str = "https://zenodo.org/record/3972451#.YkWxBn9Bzmg"
 # e.g. "https://some.com/dataset/homepage"
 
@@ -39,8 +52,14 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 
 PAPER: Optional[str] = None
 CITATION_URL: Optional[str] = "https://doi.org/10.5281/zenodo.3972451"
+AUTHORS: Optional[List[str]] = [
+    "João Pedro Augusto Costa",
+    "Omar Andres Carmona Cortes",
+    "Jose Israel de Almondes",
+]
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = "State University of Maranhão, Brazil"
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = "https://www.uema.br/"
+SLYTAGSPLIT: Optional[Dict[str, List[str]]] = None
 TAGS: List[str] = None
 
 ##################################
@@ -55,11 +74,15 @@ def check_names():
 
 
 def get_settings():
+    if RELEASE_DATE is not None:
+        global RELEASE_YEAR
+        RELEASE_YEAR = int(RELEASE_DATE.split("-")[0])
+
     settings = {
         "project_name": PROJECT_NAME,
-        "project_name_full": PROJECT_NAME_FULL,
         "license": LICENSE,
-        "industries": INDUSTRIES,
+        "applications": APPLICATIONS,
+        "category": CATEGORY,
         "cv_tasks": CV_TASKS,
         "annotation_types": ANNOTATION_TYPES,
         "release_year": RELEASE_YEAR,
@@ -67,15 +90,20 @@ def get_settings():
         "preview_image_id": PREVIEW_IMAGE_ID,
         "github_url": GITHUB_URL,
     }
+
     if any([field is None for field in settings.values()]):
         raise ValueError("Please fill all fields in settings.py after uploading to instance.")
 
+    settings["release_date"] = RELEASE_DATE
+    settings["project_name_full"] = PROJECT_NAME_FULL or PROJECT_NAME
     settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
     settings["class2color"] = CLASS2COLOR
     settings["paper"] = PAPER
     settings["citation_url"] = CITATION_URL
+    settings["authors"] = AUTHORS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
-    settings["tags"] = TAGS if TAGS is not None else []
+    settings["slytagsplit"] = SLYTAGSPLIT
+    settings["tags"] = TAGS
 
     return settings
